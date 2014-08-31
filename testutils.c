5b8cfcb778f2a43bf6c4f4193c7cca7d647cf038
Vlad <alzwded@gmail.com>
2014-08-31 20:59:52 +0300

    rename variable

--- testutils.c
+++ testutils.c
@@ -3,19 +3,19 @@
 #include <stdio.h>
 #include <math.h>
 
-static void test_printnum(vm_utilities_t utils, short (*regs)[33])
+static void test_printnum(vm_utilities_t vm, short (*regs)[33])
 {
-    short num = utils.pop();
+    short num = vm.pop();
 
     printf("Your number was: %d\n", (int)num);
 }
 
-static void test_pow(vm_utilities_t utils, short (*regs)[33])
+static void test_pow(vm_utilities_t vm, short (*regs)[33])
 {
-    short exp = utils.pop();
-    short base = utils.pop();
+    short exp = vm.pop();
+    short base = vm.pop();
     short res = pow(base, exp);
-    utils.push(res);
+    vm.push(res);
 }
 
 utility_lib_t initialize()
